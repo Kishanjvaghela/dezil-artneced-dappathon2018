905d7e90d0cfd821d7ae56d8eeeef15114baabd6
Kishan Vaghela <kishan.vaghela@finterra.org>
2018-08-18 23:37:42 +0800

    init firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var config = {
+  apiKey: 'AIzaSyAvGnGk-keJftJkcnJkR3J6z6xVqX0mQtE',
+  authDomain: 'dezil-artneced.firebaseapp.com',
+  databaseURL: 'https://dezil-artneced.firebaseio.com',
+  projectId: 'dezil-artneced',
+  storageBucket: '',
+  messagingSenderId: '402772586093'
+};
+firebase.initializeApp(config);
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
