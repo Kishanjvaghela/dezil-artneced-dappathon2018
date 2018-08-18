44750ed509cdde98b9c6e76314df5a8ddda2cbab
Kishan Vaghela <kishan.vaghela@finterra.org>
2018-08-18 22:21:54 +0800

    save tender in db

---
+++ src/database/index.js
@@ -0,0 +1,27 @@
+const createTender = (tenderId, tender) => {
+  const valueJSON = localStorage.getItem('tenders');
+  try {
+    let value = {};
+    if (valueJSON) {
+      value = JSON.parse(valueJSON);
+    }
+    value[tenderId] = tender;
+    localStorage.setItem('tenders', JSON.stringify(value));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getTenders = () => {
+  const valueJSON = localStorage.getItem('tenders');
+  console.log(valueJSON);
+  let value = {};
+  try {
+    value = JSON.parse(valueJSON);
+  } catch (e) {
+    console.log(e);
+  }
+  return value;
+};
+
+export default { createTender, getTenders };
--- src/screens/tenders/index.js
+++ src/screens/tenders/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import DataBase from '../../database';
 
 const Tenders = () => {
   return (
     <div className="pure-g">
       <div className="pure-u-1-1">
         <h1>Tenders</h1>
+        <p>{JSON.stringify(DataBase.getTenders())}</p>
       </div>
     </div>
   );
